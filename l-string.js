9ffbc31476bd198715ec5c5e43e39d5b93d79f91
leonardo.esparis.contractor <leonardo.esparis.contractor@bbva.com>
2018-08-23 13:42:19 +0200

    allow users to choose a way to interact with l-string

--- l-string.js
+++ l-string.js
@@ -1,5 +1,9 @@
-#!/usr/bin/env node
+module.exports = function(extendStringProperty) {
+	if(typeof extendStringProperty === 'boolean') {
+		if(extendStringProperty) {
+			return require('./lib/_property/extendStringProperty')();
+		}
+	}
 
-"use strict";
-
-require('./lib/extendStringProperty')();
+	return require('./lib/_lstring');
+}
