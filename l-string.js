fd9d12229907512a1b8af84f8df6cdbc52107650
Leonardo Javier Esparis Meza <ljesparis@gmail.com>
2018-08-23 20:26:32 +0200

    moving extendStringProperty.js file

--- l-string.js
+++ l-string.js
@@ -1,7 +1,7 @@
 module.exports = function(extendStringProperty) {
 	if(typeof extendStringProperty === 'boolean') {
 		if(extendStringProperty) {
-			return require('./lib/_property/extendStringProperty')();
+			return require('./lib/extendStringProperty')();
 		}
 	}
 
